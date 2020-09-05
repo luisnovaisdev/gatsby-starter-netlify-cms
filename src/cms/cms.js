85fb7a91e252a9da01abd98a8aca029c8750321d
Luis Novais <lnovais9@hotmail.com>
2020-09-05 16:13:20 +0100

    added lot changes

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,10 +1,14 @@
 import CMS from 'netlify-cms-app'
 import uploadcare from 'netlify-cms-media-library-uploadcare'
 import cloudinary from 'netlify-cms-media-library-cloudinary'
+import withStyledComponentsRendered from './styleInjector'
+
 
 import IndexPagePreview from './preview-templates/IndexPagePreview'
+
+
 
 CMS.registerMediaLibrary(uploadcare)
 CMS.registerMediaLibrary(cloudinary)
 
-CMS.registerPreviewTemplate('index', IndexPagePreview)
+CMS.registerPreviewTemplate('index', withStyledComponentsRendered(IndexPagePreview))
